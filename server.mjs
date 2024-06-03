017ec576289f6494dbb80bdfc90e9ab0421bc073
apivattua <101862813+apivattua@users.noreply.github.com>
2024-06-03 13:48:13 +0700

    testdb

--- server.mjs
+++ server.mjs
@@ -1,7 +1,11 @@
 import express from "express";
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
+
+const client = new pg.Client({ user: 'postgres', host: 'localhost', database: 'AudioPlayerApp', password: 'master151', port: 5432,});
+client.connect() .then(() => { console.log('Connected to PostgreSQL database!'); }) .catch((err) => { console.error('Error connecting to the database:', err); });
 
 app.get('/', (req, res) => {
   res.send('Welcome to my server!');
