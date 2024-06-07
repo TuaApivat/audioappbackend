b7dc3761c4c61b18ae785ed1e6cb38398219e9a4
apivattua <101862813+apivattua@users.noreply.github.com>
2024-06-07 13:34:25 +0700

    update nodejs

--- server.mjs
+++ server.mjs
@@ -1,14 +1,37 @@
 import express from "express";
 import pg from 'pg';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const client = new pg.Client({ user: 'postgres', host: 'localhost', database: 'audioplayerapp', password: 'master151', port: 5432,});
 client.connect() .then(() => { console.log('Connected to PostgreSQL database!'); }) .catch((err) => { console.error('Error connecting to the database:', err); });
 
 app.get('/', async (req, res) => {
   res.send('Welcome to my server!');
+});
+
+app.post('/api/fetchInList',async function (req, res) {
+  
+  var inputs = req.body.inputs;
+  var inputlist = inputs.split(",");
+  var statement = 'select * from music where ';
+  let n=inputlist.length;
+  for(let i=0; i<n; i++){
+    statement += 'id= ';
+    statement += inputlist[i];
+    if(i != n-1)
+      statement += ' or ';
+  }
+  var ret =  await client.query(statement);
+  console.log(statement)
+  if(ret != null){
+     res.status(200).json({'result':'success','queryret':ret.rows})
+  }
+  
 });
 
 app.get('/api/getMusic',async function (req, res) {
